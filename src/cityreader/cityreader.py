aceca9854f8104c763ea38022afe3e1bbcf8dc46
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-04-10 10:31:21 +0200

    solved cityreader.py MVP and passed all test_cityreader.py tests

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,15 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = float(lat)
+    self.lon = float(lon)
+
+  def __str__(self):
+    return f"City:({self.name}, {self.lat}, {self.lon})"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,14 +29,23 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  with open("cities.csv", "r", newline="") as f:
+    next(f, None)
+    reader = csv.reader(f)
+    for row in reader:
+      cities.append(City(row[0], row[3], row[4]))
     
-    return cities
+  return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
     print(c)
+
+# Sources:
+# https://stackoverflow.com/questions/24662571/python-import-csv-to-list
+# https://stackoverflow.com/questions/11349333/how-to-ignore-the-first-line-of-data-when-processing-csv-data
 
 # STRETCH GOAL!
 #
